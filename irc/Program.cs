f6a8d67334845bec124b70d0a7a9783921427bce
Adrian Hedqvist <adrian@tollyx.net>
2018-05-29 09:20:27 +0200

    Add project files.

---
+++ irc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace irc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
